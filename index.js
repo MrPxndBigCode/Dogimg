f09d5b2ce43e75e3fbc5344c2b57d53f262e17e0
MrPxndbigCode <chillyboy2547@gmail.com>
2025-04-20 22:24:49 +0700

    dog feature finished

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { response } from 'express';
 import bodyParser from 'body-parser';
 import axios from 'axios';
 
@@ -6,8 +6,31 @@ const port = 3000;
 const app = express();
 const API_URL = "https://dog.ceo/api/breeds/image/random";
 
-app.get("/",(req,res)=>{
-    res.render("index.ejs", { content: "Waiting for data..." });
+app.get("/",async(req,res)=>{
+    try {
+        const result = await axios.get(API_URL)
+        const imageUrl = result.data.message;
+        console.log(result.data);
+        res.render("index.ejs",{
+            data:imageUrl
+        })
+    } catch (error) {
+        console.log(error.meassage)
+    }
+})
+
+app.post("/fetch",async(req,res)=>{
+    try {
+        const result = await axios.get(API_URL)
+        const imageUrl = result.data.message;
+        console.log(result.data);
+        console.log(imageUrl);
+        res.render("index.ejs",{
+            data:imageUrl
+        })
+    } catch (error) {
+        console.log(error.meassage)
+    }
 })
 
 app.listen(port,()=>{
