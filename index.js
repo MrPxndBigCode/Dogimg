6840f29c054297a4a5e81ea6747aaf7c3473f631
MrPxndbigCode <chillyboy2547@gmail.com>
2025-04-21 00:26:45 +0700

    dog and cat finished

--- index.js
+++ index.js
@@ -5,14 +5,26 @@ import axios from 'axios';
 const port = 3000;
 const app = express();
 const API_URL = "https://dog.ceo/api/breeds/image/random";
+const API_URL_CAT = "https://api.thecatapi.com/v1/images/search?limit=1";
+const API_KEY = {
+    Headers:{
+        headers: {
+        'x-api-key':'live_viBHhcLRkR2pjxU2nfKfSacQzrSdXF6FaDRuo0xDeWFtFWl8AUgzVjJKKQsSMxXj'
+      } 
+    }
+}
 
-app.get("/",async(req,res)=>{
+app.get("",(req,res)=>{
+    res.render("index.ejs");
+})
+
+app.post("/fetch-cat",async(req,res)=>{
     try {
-        const result = await axios.get(API_URL)
-        const imageUrl = result.data.message;
-        console.log(result.data);
+        const result_cat = await axios.get(API_URL_CAT,API_KEY)
+        const imageUrl = result_cat.data[0].url;
+        console.log(result_cat.data[0].url);    
         res.render("index.ejs",{
-            data:imageUrl
+            data_cat:imageUrl
         })
     } catch (error) {
         console.log(error.meassage)
